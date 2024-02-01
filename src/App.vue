dcabe89876f6cd1178a03adcf4cb0cb49aa49438
Nivvdiy <Nivvdiy@gmail.com>
2024-02-01 16:54:09 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <LoginForm/>
+</template>
+
+<script>
+import LoginForm from './components/LoginForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LoginForm
+  }
+}
+</script>
+
+<style>
+  @import './assets/base.css';
+</style>
